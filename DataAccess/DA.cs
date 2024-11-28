4179f297701a5faa538341e7e432017864303151
leorivass <leorivas1805@gmail.com>
2024-11-28 01:06:06 -0400

    chore: Add App.config with database connection string
    -This connection string is from a test database.

--- DataAccess/DA.cs
+++ DataAccess/DA.cs
@@ -1,6 +1,5 @@
 ﻿using System.Configuration;
-using System.Data.SqlClient;
-
+using Microsoft.Data.SqlClient;
 
 namespace DataAccess
 {
@@ -12,10 +11,8 @@ namespace DataAccess
 
         public DA()
         {
-            connectionString = "Server=tcp:inmobiliariajnvc.database.windows.net,1433;Initial Catalog=inmobiliariajnvc;Persist Security Info=False;User ID=leonel;Password=twyxnztcxW1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+            connectionString = ConfigurationManager.ConnectionStrings["DBString"].ConnectionString;
             connection = new SqlConnection(connectionString);
-
         }
     }
-
 }
