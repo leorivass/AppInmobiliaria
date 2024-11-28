1606fd12eec658b881042984a1a04676c46ebefa
nicolefdz07 <n.fernandez17@unapec.edu.do>
2024-11-27 23:25:34 -0400

    DB Conexion

--- DataAccess/DA.cs
+++ DataAccess/DA.cs
@@ -1,6 +1,21 @@
-﻿namespace DataAccess
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+
+namespace DataAccess
 {
     public class DA
     {
+
+        public string connectionString = string.Empty;
+        public SqlConnection connection = new SqlConnection();
+
+        public DA()
+        {
+            connectionString = "Server=tcp:inmobiliariajnvc.database.windows.net,1433;Initial Catalog=inmobiliariajnvc;Persist Security Info=False;User ID=leonel;Password=twyxnztcxW1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+            connection = new SqlConnection(connectionString);
+
+        }
     }
+
 }
